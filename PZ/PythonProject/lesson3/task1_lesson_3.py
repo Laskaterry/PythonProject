819f80540d842edb8ff7e071281ca38a6d3c64bf
Kseniya <darthrosetta@gmail.com>
2021-11-16 17:51:35 +0300

    lesson_2

---
+++ PZ/PythonProject/lesson3/task1_lesson_3.py
@@ -0,0 +1,19 @@
+def num_translate(word):
+    translate = {"one": "один",
+                 "two": "два",
+                 "three": "три",
+                 "four": "четыре",
+                 "five": "пять",
+                 "six": "шесть",
+                 "seven": "семь",
+                 "eight": "восемь",
+                 "nine": "девять",
+                 "ten": "десять",
+                 "zero": "ноль"}
+    if word[0].isupper() and translate.get(word.lower()):
+        return translate.get(word.lower()).title()
+    else:
+        return translate.get(word)
+
+
+print(num_translate(input("Введите число по-английски для перевода: ")))
