ff56d4721c16f8bf1c347563df339104af1ea945
Laskaterry <88809500+Laskaterry@users.noreply.github.com>
2021-12-03 18:17:17 +0300

    Delete task5_lesson_5.py

--- PZ/PythonProject/lesson5/task5_lesson_5.py
+++
@@ -1,9 +0,0 @@
-src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-nums = {}
-for i in src:
-    if i in nums:
-        nums[i] += 1
-    else:
-        nums[i] = 1
-nums_res = [i for i in src if nums[i] == 1]
-print(nums_res)
